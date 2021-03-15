4b78ad77eb915c0dbacda520d11f6b1db9d9b94b
Gareth Hudson <garethhudson07@gmail.com>
2021-03-15 16:33:39 +0000

    expose schema collection

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import Repository from './repository';
 import Transformer from './transformer';
 import Schema from './schema/schema';
+import Collection from './schema/collection';
 import ErrorBag from './errors/bag';
 import RestModelError from './errors/rest-model';
 
@@ -8,6 +9,7 @@ export {
     Repository,
     Transformer,
     Schema,
+    Collection,
     ErrorBag,
     RestModelError
 };
