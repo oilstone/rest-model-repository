30278c5283ab8aea2850632d284bea81fdb15606
Brendan Bullen <66941+bullenb@users.noreply.github.com>
2022-03-23 12:52:35 +0000

    Remove schema object references
    Signed-off-by: Brendan Bullen <66941+bullenb@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 import Repository from './repository';
-import Schema from './schema/schema';
-import Collection from './schema/collection';
 import ErrorBag from './errors/bag';
 import RestModelError from './errors/rest-model';
 import Transformer from './transformers/transformer';
@@ -8,8 +6,6 @@ import Transformer from './transformers/transformer';
 export {
     Repository,
     Transformer,
-    Schema,
-    Collection,
     ErrorBag,
     RestModelError
 };
